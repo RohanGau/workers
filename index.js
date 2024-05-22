a4a3d700b6f2c2f7452cb3e3929a5ddf51845b9c
Rohan kumar <46267198+RohanGau@users.noreply.github.com>
2024-05-22 10:04:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,3 +19,18 @@ function stopWorker() {
   worker.terminate();
   worker = undefined;
 }
+
+
+// ***** service worker registeration *******
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then(registration => {
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      })
+      .catch(error => {
+        console.log('ServiceWorker registration failed: ', error);
+      });
+  });
+}
