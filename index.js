5a4ffe5b4b297e601ef3fdecb9e65804437a493d
Rohan kumar <46267198+RohanGau@users.noreply.github.com>
2024-05-23 00:55:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -34,3 +34,23 @@ if ('serviceWorker' in navigator) {
       });
   });
 }
+
+// **** Enabled push notification and need to subscribed to user 
+
+if("Notification" in window && navigator.serviceWorker) {
+  console.log("Notification");
+  Notification.requestPermission(permission => {
+    if(permission === "granted") {
+      navigator.serviceWorker.ready.then(registration => {
+        registration.pushManager.subscribe({
+          userVisibleOnly: true,
+          applicationServerKey: "YOUR_PUBLIC_VAPID_KEY"
+        }).then((subscribe) => {
+          console.log("User is subscribed :", subscribe);
+        }).catch((error) => {
+          console.log("Failed to subscribed to user :", error);
+        })
+      }); 
+    }
+  });
+}
